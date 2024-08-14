db1b9caa442db3d5bcacfcd2401f24396df47fc5
tsnbluemacky <tsnbluemacky@gmail.com>
2024-08-14 14:34:03 +0900

    ユーザー新規登録・ログイン・ログアウト実装確認

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,3 @@
-import Rails from "@rails/ujs"
-Rails.start()
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
